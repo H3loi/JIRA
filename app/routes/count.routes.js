76aae86fcda8d931a36c17e723c8f1e47f311fc7
H3loi <maria.eloiza.reyes.sitesphil.ojt@gmail.com>
2025-03-28 16:56:17 +0800

    Delete app directory

--- app/routes/count.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const countController = require("../controllers/count.controller");
-
-router.get("/counts", countController.getCounts);
-
-module.exports = router;
--- app/routes/notification.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const notificationController = require("../controllers/notification.controller");
-
-router.post("/send-email", notificationController.sendEmailNotification);
-
-module.exports = router;
